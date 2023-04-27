f0b5628dc0c7618de3cc01f7a084c1f99f7e1b87
James.R <hilarionjames25@gmail.com>
2023-04-26 22:04:51 -0400

    pushing final project files

---
+++ EcommerceAPI/Models/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace EcommerceAPI.Models
+{
+    public class Response
+    {
+        public int statusCode { get; set; }
+        public string? statusDescription { get; set; }
+
+        public List<Discount>? discounts { get; set; }
+        public List<Product>? products { get; set; }
+        public List<Review>? reviews { get; set; }
+
+        public Discount? discount { get; set; }
+        public Product? product { get; set; }
+        public Review? review { get; set; }
+    }
+}
+
